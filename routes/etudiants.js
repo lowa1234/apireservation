77ce3ee4b98c167ef8dcc7c213ae6f2d21fa3d45
lowa1234 <johnphilipps90@gmail.com>
2021-10-06 15:49:17 -0400

    Update etudiants.js

--- routes/etudiants.js
+++ routes/etudiants.js
@@ -16,12 +16,12 @@ router.get('/', async(req, res) => {
     }
 });
 
-router.get('/sku/:sku', async(req, res) =>{
+router.get('/:da', async(req, res) =>{
     await mongoose.connect(process.env.MONGODB_APP_URI);
     try{
-        const produit = await Produit.findOne({sku: req.params.sku});
-        if(!produit){
-            res.status(404).json({erreur: 'Produit introuvable'});
+        const etudiant = await Etudiant.findOne({da: req.params.da});
+        if(!etudiant){
+            res.status(404).json({erreur: 'Étudiant introuvable'});
         }
         else{
             res.json(produit);
@@ -34,68 +34,12 @@ router.get('/sku/:sku', async(req, res) =>{
     }
 });
 
-router.get('/variants/:sku', async(req, res) =>{
-    await mongoose.connect(process.env.MONGODB_APP_URI);
-    try{
-        const produits = await Produit.find({sku: req.params.sku}, 'variants');
-        if(!produits){
-            res.status(404).json({erreur: 'Produits introuvables'});
-        }
-        else{
-            res.json(produits);
-        }
-    } catch(err){
-        console.log(err);
-        res.status(500).json({erreur: 'Une erreur est survenue...'});
-    } finally{
-        mongoose.connection.close();
-    }
-});
-
-router.get('/:categorie/qtenegative', async(req, res) =>{
-    await mongoose.connect(process.env.MONGODB_APP_URI);
-    try{
-        const produits = await Produit.find({categories: req.params.categorie}).where('stock').lt(0);
-        if(!produits){
-            res.status(404).json({erreur: 'Produits introuvables'});
-        }
-        else{
-            res.json(produits);
-        }
-    } catch(err){
-        console.log(err);
-        res.status(500).json({erreur: 'Une erreur est survenue...'});
-    } finally{
-        mongoose.connection.close();
-    }
-});
-
-router.get('/stats/prix-moyen-par-statut', async(req, res) =>{
-    await mongoose.connect(process.env.MONGODB_APP_URI);
-    try{
-        const produit = await Produit.aggregate([
-            {"$group": {
-                "_id":"$statut",
-                "prixMoyen": {"$avg":"$prix"}
-                }
-            },
-            {"$sort": {"prixMoyen": 1}}
-        ]);
-        res.json(produit);
-    } catch(err){
-        console.log(err);
-        res.status(500).json({erreur: 'Une erreur est survenue...'});
-    } finally{
-        mongoose.connection.close();
-    }
-});
-
 router.post('/', async(req, res) =>{
     await mongoose.connect(process.env.MONGODB_APP_URI);
     try{
-        const produit = new Produit(req.body);
-        const nouveauProduit = await produit.save();
-        res.json(nouveauProduit);
+        const etudiant = new Etudiant(req.body);
+        const nouvelEtudiant = await etudiant.save();
+        res.json(nouvelEtudiant);
     } catch(err){
         console.log(err);
         res.status(500).json({erreur: 'Une erreur est survenue...'});
