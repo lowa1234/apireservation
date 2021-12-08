e1fc5f24df40c4d61d19d24a7a746cf8aba9a443
lowa1234 <johnphilipps90@gmail.com>
2021-12-08 16:18:28 -0500

    Ajout du maxlength du da

--- models/etudiant.js
+++ models/etudiant.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const EtudiantSchema = new Schema({
     nom: {type: String, required: true, maxlength: 50},
     prenom: {type: String, required: true, maxlength: 50},
-    da: {type: String, required: true},
+    da: {type: String, required: true, maxlength: 7},
     amis: {
         nom: {type: String, required: true, maxlength: 50},
         prenom: {type: String, required: true, maxlength: 50},
