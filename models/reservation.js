9ffd256e17544137fffdcfbd59bff44e73e9b82c
lowa1234 <johnphilipps90@gmail.com>
2021-12-17 20:16:20 -0500

    Update reservation.js

--- models/reservation.js
+++ models/reservation.js
@@ -4,8 +4,19 @@ const Schema = mongoose.Schema;
 const ReservationSchema = new Schema({
     da_etudiant: {type: String, required: true, minlength: [7, 'Le DA doit être composé de 7 chiffres'], maxlength: [7, 'Le DA doit être composé de 7 chiffres']},
     nom_local: {type: String, required: true, minlength: [5, 'Le nom doit contenir 5 caractère ex. C-205'], maxlength: [5, 'Le nom doit contenir 5 caractère ex. C-205']},
-    heure_debut: {type: Date, required: true},
-    heure_fin: {type: Date, required: true}
+    date: {type: Date, required: true},
+    heure_debut: {type: String, required: true, validate: {
+        validator: function(v) {
+          return /\d{2}:\d{2}:\d{2}/.test(v);
+        },
+        message: props => `${props.value} n'est pas une heure valide!`
+      }},
+    heure_fin: {type: String, required: true, validate: {
+        validator: function(v) {
+          return /\d{2}:\d{2}:\d{2}/.test(v);
+        },
+        message: props => `${props.value} n'est pas une heure valide!`
+      }}
 });
 
 module.exports = mongoose.model('Reservation', ReservationSchema);
